b1b250d7ef4582152f526df9162ec0643b47a42a
Snigdha Tiwari <snigdha2210@gmail.com>
2025-01-06 19:09:57 -0500

    added icon image and background.js file

---
+++ background.js
@@ -0,0 +1,29 @@
+// Listener for extension installed or updated events
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('YouTube Caption Search extension installed.');
+});
+
+// Listener to handle messages from popup.js or content scripts
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'getCaptions') {
+    // Send a command to the content script to fetch captions
+    chrome.tabs.sendMessage(
+      sender.tab.id,
+      { action: 'fetchCaptions' },
+      (response) => {
+        if (response && response.captions) {
+          sendResponse({ captions: response.captions });
+        } else {
+          sendResponse({ captions: [] });
+        }
+      }
+    );
+    return true; // Indicates async response
+  }
+});
+
+// Handle any action clicks or injected scripts
+chrome.action.onClicked.addListener((tab) => {
+  // Open popup if required or toggle some feature
+  console.log('Extension action clicked on tab:', tab);
+});
